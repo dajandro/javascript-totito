f209b51fc125707af18c74f9c43d29ca5536e4d3
dajandro <dajandro95@gmail.com>
2016-08-09 18:24:37 -0600

    Object state
    Change var to attributes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,52 +6,61 @@
 */
 
 /*
-state = 0: X
-state = 1: O
+state.turno = 0: X
+state.turno = 1: O
 */
 
 // Modelo / estado
-var state = 0;
-var end = false
-var empate = false;
-var tiro = -1;
-var tableroMatrix = [[-1,-1,-1],[-1,-1,-1],[-1,-1,-1]];
-var tableroImgs = [['', '', ''],['', '', ''],['', '', '']];
-var ganadorImg = '';
+var state = {
+  turno: 0,
+  end: false,
+  empate: false,
+  tableroMatrix: [
+    [-1,-1,-1],
+    [-1,-1,-1],
+    [-1,-1,-1]
+  ],
+  tableroImgs: [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+  ],
+  ganadorImg: ''
+};
 
 var viewport = document.getElementById("viewport");
 
-function render(){
+function render(state){
   var html = '<div class="indicadores">';
      html += '  <div class="viewport" id="turno">';
-     if(state == 0){
+     if(state.turno == 0){
       html += 'Turno<br><img src="images/x.png" id="turno-img">';
      }
      else{
       html += 'Turno<br><img src="images/o.png" id="turno-img">';
      }
      html += "</div>"
-     if (empate == true)
+     if (state.empate == true)
       html += '  <div class="viewport" id="ganador">Empate</div>';
      else
-      html += '  <div class="viewport" id="ganador">Ganador<br>' + ganadorImg + '</div>';
+      html += '  <div class="viewport" id="ganador">Ganador<br>' + state.ganadorImg + '</div>';
      html += '</div>';
      html += '<div class="container">';
      html += '  <table id="tablero">';
      html += '    <tr>';
-     html += '      <td class="casilla" id="viewport-11">' + tableroImgs[0][0] + '</td>';
-     html += '      <td class="casilla" id="viewport-12">' + tableroImgs[0][1] + '</td>';
-     html += '      <td class="casilla" id="viewport-13">' + tableroImgs[0][2] + '</td>';
+     html += '      <td class="casilla" id="viewport-11">' + state.tableroImgs[0][0] + '</td>';
+     html += '      <td class="casilla" id="viewport-12">' + state.tableroImgs[0][1] + '</td>';
+     html += '      <td class="casilla" id="viewport-13">' + state.tableroImgs[0][2] + '</td>';
      html += '    </tr>';
      html += '    <tr>';
-     html += '      <td class="casilla" id="viewport-21">' + tableroImgs[1][0] + '</td>';
-     html += '      <td class="casilla" id="viewport-22">' + tableroImgs[1][1] + '</td>';
-     html += '      <td class="casilla" id="viewport-23">' + tableroImgs[1][2] + '</td>';
+     html += '      <td class="casilla" id="viewport-21">' + state.tableroImgs[1][0] + '</td>';
+     html += '      <td class="casilla" id="viewport-22">' + state.tableroImgs[1][1] + '</td>';
+     html += '      <td class="casilla" id="viewport-23">' + state.tableroImgs[1][2] + '</td>';
      html += '    </tr>';
      html += '    <tr>';
-     html += '      <td class="casilla" id="viewport-31">' + tableroImgs[2][0] + '</td>';
-     html += '      <td class="casilla" id="viewport-32">' + tableroImgs[2][1] + '</td>';
-     html += '      <td class="casilla" id="viewport-33">' + tableroImgs[2][2] + '</td>';
+     html += '      <td class="casilla" id="viewport-31">' + state.tableroImgs[2][0] + '</td>';
+     html += '      <td class="casilla" id="viewport-32">' + state.tableroImgs[2][1] + '</td>';
+     html += '      <td class="casilla" id="viewport-33">' + state.tableroImgs[2][2] + '</td>';
      html += '    </tr>';
      html += '  </table>';
      html += '</div>';
@@ -59,7 +68,7 @@ function render(){
   return html;
 }
 
-viewport.innerHTML = render();
+viewport.innerHTML = render(state);
 
 changeTrigger = document.getElementById("reset");
 changeTrigger.addEventListener("click", reset);
@@ -70,7 +79,7 @@ for (i = 0; i < casillas.length; i++){
 
 function imagePlayer(){
   var html = "";
-  if(state == 0){
+  if(state.turno == 0){
     html = '<img src="images/x.png" id="turno-img">';    
   }
   else{
@@ -86,7 +95,7 @@ function casilla()
   var itemImg = item.innerHTML;
   var error = false;
   // Verificar si la casilla esta ocupada o el juego ya llego al final
-  if ( (itemImg != '') || (end == true) )
+  if ( (itemImg != '') || (state.end == true) )
     error = true;
   // Si no ha llegado al final
   if (! error)
@@ -94,32 +103,32 @@ function casilla()
     // Se pone el valor en la matriz de la casilla correspondiente
     var fila = itemId.substring(9,10)-1;
     var columna = itemId.substring(10,11)-1;
-    tableroMatrix[fila][columna] = state;
+    state.tableroMatrix[fila][columna] = state.turno;
     // Se pone la imagen en la casilla
-    tableroImgs[fila][columna] = imagePlayer();
+    state.tableroImgs[fila][columna] = imagePlayer();
     // Verificar si hay ganador
     var win = ganador();
     if (! win){
       if (isTableroFull()){
-        empate = true;
-        end = true;
+        state.empate = true;
+        state.end = true;
       }
       else{
         // Se cambia de estado
-        if(state == 0){
-          state = 1;
+        if(state.turno == 0){
+          state.turno = 1;
         }
-        else if(state == 1){
-          state = 0;  
+        else if(state.turno == 1){
+          state.turno = 0;  
         }        
       }      
     }
     else{
-      end = true;
-      ganadorImg = imagePlayer();
+      state.end = true;
+      state.ganadorImg = imagePlayer();
     }    
   }
-  viewport.innerHTML = render();
+  viewport.innerHTML = render(state);
   changeTrigger = document.getElementById("reset");
   changeTrigger.addEventListener("click", reset);
   casillas = document.getElementsByClassName("casilla");
@@ -132,16 +141,16 @@ function casilla()
 
 function reset(){
   // Estado inicial
-  state = 0;
+  state.turno = 0;
   // Reiniciar matriz de juego
-  tableroMatrix = [[-1,-1,-1],[-1,-1,-1],[-1,-1,-1]];
-  tableroImgs = [['', '', ''],['', '', ''],['', '', '']];
-  ganadorImg = '';
+  state.tableroMatrix = [[-1,-1,-1],[-1,-1,-1],[-1,-1,-1]];
+  state.tableroImgs = [['', '', ''],['', '', ''],['', '', '']];
+  state.ganadorImg = '';
   // Reset flags
-  end = false;
-  empate = false;
+  state.end = false;
+  state.empate = false;
   // Aplicar renderTurno al estado correspondiente
-  viewport.innerHTML = render();
+  viewport.innerHTML = render(state);
   changeTrigger = document.getElementById("reset");
   changeTrigger.addEventListener("click", reset);
   casillas = document.getElementsByClassName("casilla");
@@ -157,8 +166,8 @@ function ganador(){
     var suma = -1;
     var tiros = 0;
     for(j = 0; j < 3; j++){ 
-      suma += tableroMatrix[i][j];
-      if (tableroMatrix[i][j] != -1)
+      suma += state.tableroMatrix[i][j];
+      if (state.tableroMatrix[i][j] != -1)
         tiros++;
     }    
     if ( ((suma == 2) || (suma == -1) ) && (tiros == 3) ){
@@ -171,8 +180,8 @@ function ganador(){
     var suma = -1;
     var tiros = 0;
     for(j = 0; j < 3; j++){ 
-      suma += tableroMatrix[j][i];
-      if (tableroMatrix[j][i] != -1)
+      suma += state.tableroMatrix[j][i];
+      if (state.tableroMatrix[j][i] != -1)
         tiros++;
     }    
     if ( ((suma == 2) || (suma == -1) ) && (tiros == 3) ){
@@ -183,27 +192,27 @@ function ganador(){
   // Validacioes diagonales
   var suma = -1;
   var tiros = 0;
-  suma += tableroMatrix[0][0];  
-  suma += tableroMatrix[1][1];
-  suma += tableroMatrix[2][2];
-  if (tableroMatrix[0][0] != -1)
+  suma += state.tableroMatrix[0][0];  
+  suma += state.tableroMatrix[1][1];
+  suma += state.tableroMatrix[2][2];
+  if (state.tableroMatrix[0][0] != -1)
     tiros++;
-  if (tableroMatrix[1][1] != -1)
+  if (state.tableroMatrix[1][1] != -1)
     tiros++;
-  if (tableroMatrix[2][2] != -1)
+  if (state.tableroMatrix[2][2] != -1)
     tiros++;
   if ( ((suma == 2) || (suma == -1) ) && (tiros == 3) )
     rtrn = true;
   suma = -1;
   tiros = 0;
-  suma += tableroMatrix[0][2];
-  suma += tableroMatrix[1][1];
-  suma += tableroMatrix[2][0];
-  if (tableroMatrix[0][2] != -1)
+  suma += state.tableroMatrix[0][2];
+  suma += state.tableroMatrix[1][1];
+  suma += state.tableroMatrix[2][0];
+  if (state.tableroMatrix[0][2] != -1)
     tiros++;
-  if (tableroMatrix[1][1] != -1)
+  if (state.tableroMatrix[1][1] != -1)
     tiros++;
-  if (tableroMatrix[2][0] != -1)
+  if (state.tableroMatrix[2][0] != -1)
     tiros++;
   if ( ((suma == 2) || (suma == -1) ) && (tiros == 3) )
     rtrn = true;
@@ -215,7 +224,7 @@ function isTableroFull()
   var tiros = 0;
   for (i = 0; i < 3; i++){
     for(j = 0; j < 3; j++){      
-      if (tableroMatrix[i][j] != -1)
+      if (state.tableroMatrix[i][j] != -1)
         tiros++;
       else
         break;
